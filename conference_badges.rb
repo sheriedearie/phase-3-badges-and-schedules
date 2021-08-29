af4d1460436cff6178c2d4067de4b4c169230416
Sheridan <sheridanmaustad@gmail.com>
2021-08-28 18:41:34 -0600

    commit tree 6b750efee5c49498095f0bf0ee21d4a388c0120f

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+  end
+
+def batch_badge_creator(people)
+    people.map do |people|
+        "Hello, my name is #{people}."
+    end
+end
+
+def assign_rooms(people)
+    people.map.with_index(1) do |people, room_num|
+        "Hello, #{people}! You'll be assigned to room #{room_num}!"
+    end
+end
+
+def printer(people)
+    batch_badge_creator(people).each do |badge|
+        puts badge
+    end
+
+    assign_rooms(people).each do |assignment|
+        puts assignment
+    end
+end
